06409461c33a469abed49724e8ee8ca0707e393e
Alejandro Álvarez <thealex11011999@gmail.com>
2019-02-25 10:43:36 +0100

    Completado

---
+++ src/ejercicios/IlegalTriangleException.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+@SuppressWarnings("serial")
+public class IlegalTriangleException extends Exception {
+
+	public IlegalTriangleException(String message) {
+		super(message);
+		
+	}
+
+}
